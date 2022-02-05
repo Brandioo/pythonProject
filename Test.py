eaf5c5d65a77966e9fe4451307c84994cfabc2e4
Brand <citozibrand@gmail.com>
2022-02-05 13:13:41 +0100

    Python Project

---
+++ Test.py
@@ -0,0 +1,8 @@
+a=22
+b=60
+c=0
+
+if a>23 and c==0:
+    print("ssd")
+else:
+    print ("No")
---
+++ Test1.py
@@ -0,0 +1,11 @@
+a = "22"
+b = int(a)
+c = 90
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(b)
+
+# OK
