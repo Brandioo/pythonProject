ee131df3d12118b7273e5278f3b97fd46487fee7
Brand <citozibrand@gmail.com>
2022-02-06 14:44:44 +0100

    Print Types

--- printTypes.py
+++ printTypes.py
@@ -2,7 +2,20 @@ print("What", "a", "beautiful", "day", ".", sep="-")
 print("-----------------------------------------")
 print("What", "a", "beautiful", "day", ".", sep="\n")
 print("-----------------------------------------")
-print("1", "2", 3, 4, 5)
+print("What", "a", "beautiful", "day", ".", sep="\t")
+print("-----------------------------------------")
+print("What", "a", "beautiful", "day", ".", sep="\n", end="! \n")
+print("-----------------------------------------")
+print("1", "2", 3, 4, 5,sep=",")
 print("-----------------------------------------")
 fruit = "orange"
 print(fruit)
+
+
+title="General"
+name="Kenobi"
+age=28
+print("Hello, %s %s ", (title,name))
+print("Hello there, {} {}".format(title, name))
+print("Hello there", {title}, {name}, {age})
+print(f"Hello there {title}, {name}, {age}")
