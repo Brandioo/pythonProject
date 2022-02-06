90d7f1c8772fd457396e47c70bb20e028e664ca6
Brand <citozibrand@gmail.com>
2022-02-06 11:55:53 +0100

    For Loops And Interations

---
+++ Interation.py
@@ -0,0 +1,10 @@
+names = ["John", "Ana", "Guido", "Test"]
+
+# greeting = "Hello " + name[0] + ", " + name[1] + ", " + name[2] + ", " + name[3]
+# print(greeting)
+# for number in range(10):
+#     print(number)
+
+greeting = "Hello "
+for name in names:
+    print(greeting+name)
