86df112eac27e5caa65614946cf8244302125ece
Brand <citozibrand@gmail.com>
2022-02-06 12:08:24 +0100

    Ranges

--- Interation.py
+++ Interation.py
@@ -1,10 +1,8 @@
-names = ["John", "Ana", "Guido", "Test"]
+# names = ["John", "Ana", "Guido", "Test"]
 
 # greeting = "Hello " + name[0] + ", " + name[1] + ", " + name[2] + ", " + name[3]
 # print(greeting)
-# for number in range(10):
-#     print(number)
 
-greeting = "Hello "
-for name in names:
-    print(greeting+name)
+# greeting = "Hello "
+# for name in names:
+#     print(greeting+name)
---
+++ RangeTypes.py
@@ -0,0 +1,5 @@
+for num in range(1, 11, 2):
+    print(num)
+
+# for number in range(10):
+#     print(number)
