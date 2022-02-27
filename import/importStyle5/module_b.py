6a200e92de974ad8e9a56e00dc1c7d98e7f392f7
Brand <citozibrand@gmail.com>
2022-02-27 12:23:01 +0100

    ImportStyles

--- import/importStyle5/module_b.py
+++ import/importStyle5/module_b.py
@@ -1,10 +1,7 @@
 # We import all variables and functions
-from module_a import *
+from module_a import find_index as fi
 
 # print(test)
 courses = ["PE", "History", "Math"]
-index = find_index(courses, "PE")
+index = fi(courses, "PE")
 print(index)  # Prints 0
-
-printType = printTest()
-print(printType)
